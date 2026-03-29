361e44782ea093d6335ed28bc265172ad63d28d8
Aditya Tayade <tayadeaditya451@gmail.com>
2026-03-29 17:18:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let p1 = Math.floor(Math.random() * 6) + 1;
+let p2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "dice" + p1 + ".png");
+document.querySelector(".img2").setAttribute("src", "dice" + p2 + ".png");
+
+if (p1 > p2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (p2 > p1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "It's a Draw!";
+}
